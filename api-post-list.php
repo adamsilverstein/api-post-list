2153bb2c78415cceccbb105a2dc725b2ab9d5159
Adam Silverstein <adam@10up.com>
2016-08-07 23:36:29 -0400

    Better data setup, prep for comments

--- api-post-list.php
+++ api-post-list.php
@@ -14,27 +14,39 @@
 define( 'API_POST_LIST_URL', plugin_dir_url( __FILE__ ) );
 define( 'API_POST_LIST_VERSION', '0.0.1' );
 
+
+
+
 function api_post_list_scripts() {
 
 }
+add_action( 'wp_scripts', 'api_post_list_scripts' );
+
+
+
 
 function api_post_list_shortcode( $atts ) {
+	wp_enqueue_script( 'api_pl', API_POST_LIST_URL . 'js/api-pl.js', array( 'wp-backbone', 'wp-api' ), API_POST_LIST_VERSION, true );
 
-	wp_enqueue_script( 'api_pl', API_POST_LIST_URL . 'js/api-pl.js', array( 'wp-api' ), API_POST_LIST_VERSION, true );
 	$rand_id = rand();
-?>
-	<script type="text/javascript">
-	var postListData<?php echo esc_attr( $rand_id ); ?> = <?php echo json_encode( $atts ); ?>;
-	</script>
-	<div class="api-post-list-container" data-plid="" ><?php echo esc_attr( $rand_id ); ?></div>
-	<script type="text/html" id="tmpl-single-post">
-		<div class="api-post-list-image">
-			<img src="{{ data.imageSrc }}" />
-		</div>
-		<div class="api-post-list-title">
-			{{ data.title }}
-		</div>
-	</script>
-<?
+	return '<div class="api-post-list-container" data-plid="' . esc_attr( $rand_id ) . '" data-posts="' . esc_attr( json_encode( $atts ) ) .'"></div>';
 }
 add_shortcode( 'api_post_list', 'api_post_list_shortcode' );
+
+
+
+
+
+function api_post_list_backbone_tempalates( ) {
+	?>
+	<script type="text/html" id="tmpl-single-post">
+			<# console.log( data.attributes ); #>
+		<div class="api-post-list-image">
+ 		</div>
+		<div class="api-post-list-title">
+		{{ data.attributes.title.rendered }}
+		</div>
+	</script>
+	<?php
+}
+add_action( 'wp_print_footer_scripts', 'api_post_list_backbone_tempalates' );
